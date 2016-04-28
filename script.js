86b19d7d9361ffefa1ed93cfa9eac1e72cc426d9
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-04-27 23:29:29 -0500

    WIP refactoring half way

--- script.js
+++ script.js
@@ -35,25 +35,80 @@ var move = 0
 
 function selectMove() {
   //Define symbol and write to board
-  var symbol = "";
-  if(turnSwitch % 2 === 0){
-    symbol = "X";
-  } else {
-    symbol = "O";
-  }
+  var symbol = playerSymbol();
+
+  //Set the tile to the player's symbol
   this.innerHTML = symbol;
 
   //Remove the event listener for that tile
   this.removeEventListener("click", selectMove);
 
-  if(symbol === "X"){
-    this.classList.add("place-x");
-  } else {
-    this.classList.add("place-o");
-  }
+  //Sets the symbol to red or green
+  symbolColor(symbol, this);
 
   //Populate array
-  switch(this.id) {
+  populateArray(this, symbol);
+
+  //Check for winner
+  var win = false;
+  win = checkForWinner(symbol);
+
+  //Tick up turnSwither
+  turnSwitch++;
+
+  //if win show result else prompt for next turn
+  if(win){
+    //Remove the player X and player O ready message
+    hideXReady();
+    hideOReady();
+
+    //Add Winner message
+    addWinnerMessage(symbol);
+
+    //Remove Event listeners
+    var tiles = document.getElementsByClassName("board-tile");
+    for(var i = 0; i < tiles.length; i++){
+      tiles[i].removeEventListener("click", selectMove);
+    };
+  } else if(move === 8) {
+    //Display tie message and remove whose turn it is
+    var tieMessage = document.getElementById("tie-game");
+    addClass(tieMessage, "visible");
+    removeClass(tieMessage, "hidden");
+    hideXReady();
+    hideOReady();
+  } else {
+    //Display who's turn it is.
+    playerToggle();
+  }
+
+  //Increment the number of moves by 1
+  move++;
+}
+
+//Sets the current players symbol
+//Returns a string representing the players symbol, 'X' or 'O'
+function playerSymbol(){
+  if(turnSwitch % 2 === 0){
+    symbol = "X";
+  } else {
+    symbol = "O";
+  }
+  return symbol
+}
+
+//Adds class to change X to red or O to green
+function symbolColor(symbol, tile){
+  if(symbol === "X"){
+    tile.classList.add("place-x");
+  } else {
+    tile.classList.add("place-o");
+  };
+};
+
+//Populate array with the appropriate symbol
+function populateArray(tile, symbol){
+  switch(tile.id) {
     case "tile-1-1":
       board[0][0] = symbol;
       break;
@@ -81,110 +136,83 @@ function selectMove() {
     case "tile-3-3":
       board[2][2] = symbol;
       break;
-  }
+  };
+};
 
-  //Check for winner
-  var win = false;
-  //Check for horizontal winner
-  win =     board[0][0] === symbol && board[0][1] === symbol && board[0][2] === symbol || //first row
-            board[1][0] === symbol && board[1][1] === symbol && board[1][2] === symbol || //second row
-            board[2][0] === symbol && board[2][1] === symbol && board[2][2] === symbol || //third row
-            board[0][0] === symbol && board[1][0] === symbol && board[2][0] === symbol || //first column
-            board[0][1] === symbol && board[1][1] === symbol && board[2][1] === symbol || //second column
-            board[0][2] === symbol && board[1][2] === symbol && board[2][2] === symbol || //third column
-            board[0][0] === symbol && board[1][1] === symbol && board[2][2] === symbol || // 1st diagnol
-            board[2][0] === symbol && board[1][1] === symbol && board[0][2] === symbol;   // 2nd diagnol
-  //Tick up turnSwither
-  turnSwitch++;
+//Check each combination of paths for a winning set
+// +symbol: a string representing "X" or "O"
+//Returns a boolean representing if there is a winner or not
+function checkForWinner(symbol) {
+  win = board[0][0] === symbol && board[0][1] === symbol && board[0][2] === symbol || //first row
+        board[1][0] === symbol && board[1][1] === symbol && board[1][2] === symbol || //second row
+        board[2][0] === symbol && board[2][1] === symbol && board[2][2] === symbol || //third row
+        board[0][0] === symbol && board[1][0] === symbol && board[2][0] === symbol || //first column
+        board[0][1] === symbol && board[1][1] === symbol && board[2][1] === symbol || //second column
+        board[0][2] === symbol && board[1][2] === symbol && board[2][2] === symbol || //third column
+        board[0][0] === symbol && board[1][1] === symbol && board[2][2] === symbol || // 1st diagnol
+        board[2][0] === symbol && board[1][1] === symbol && board[0][2] === symbol;   // 2nd diagnol
+  return win;
+};
 
-  //if win show result else prompt for next turn
-  if(win){
-    //Remove the X or O ready message
-    if(symbol === "X"){
-      var xReady = document.getElementById("player-x-ready");
-      removeClass(xReady, "visible");
-      addClass(xReady, "hidden");
-    } else {
-      var oReady = document.getElementById("player-o-ready");
-      removeClass(oReady, "visible");
-      addClass(oReady, "hidden");
-    }
-    //Add Winner message
-    if(symbol === "X"){
-      var xWin = document.getElementById("player-x-wins");
-      removeClass(xWin, "hidden");
-      addClass(xWin, "visible");
-      //Increment X player wins by 1
-      xPlayerWins++
-      //Display x player wins in html
-      var xScore = document.getElementById("x-score");
-      xScore.innerHTML = xPlayerWins;
-    } else {
-      var oWin = document.getElementById("player-o-wins");
-      removeClass(oWin, "hidden");
-      addClass(oWin, "visible");
-      //Increment O player wins by 1
-      oPlayerWins++
-      //Display 0 player wins in html
-      var oScore = document.getElementById("o-score");
-      oScore.innerHTML = oPlayerWins;
-    }
+//Hide the div with class name of 'player-x-ready'
+function hideXReady(){
+  var xReady = document.getElementById("player-x-ready");
+  removeClass(xReady, "visible");
+  addClass(xReady, "hidden");
+};
 
-    //Remove Event listeners
-    var tiles = document.getElementsByClassName("board-tile");
-    for(var i = 0; i < tiles.length; i++){
-      tiles[i].removeEventListener("click", selectMove);
-    };
-  } else if(move === 8) {
-    //Display tie message and remove whose turn it is
-    var tieMessage = document.getElementById("tie-game");
-    addClass(tieMessage, "visible");
-    removeClass(tieMessage, "hidden");
-    var xReady = document.getElementById("player-x-ready");
-    removeClass(xReady, "visible");
-    addClass(xReady, "hidden");
-    var oReady = document.getElementById("player-o-ready");
-    removeClass(oReady, "visible");
-    addClass(oReady, "hidden");
-  } else {
-    //Display who's turn it is.  Even = x  Odd = o
-    if(turnSwitch % 2 === 0){
-      var xReady = document.getElementById("player-x-ready");
-      addClass(xReady, "visible");
-      removeClass(xReady, "hidden");
-      var oIdle = document.getElementById("player-o-ready");
-      addClass(oIdle, "hidden");
-      removeClass(oIdle, "visible");
-    } else {
-      var oReady = document.getElementById("player-o-ready");
-      addClass(oReady, "visible");
-      removeClass(oReady, "hidden");
-      var xIdle = document.getElementById("player-x-ready");
-      addClass(xIdle, "hidden");
-      removeClass(xIdle, "visible");
-    };
-  }
-  move++;
-}
+//Hide the div with class name of 'player-o-ready'
+function hideOReady(){
+  var oReady = document.getElementById("player-o-ready");
+  removeClass(oReady, "visible");
+  addClass(oReady, "hidden");
+};
 
-function playerToggle(turnSwitch){
-  //Display who's turn it is.  Even = x  Odd = o
+//Show the div with the class name of 'player-x-ready'
+function showXReady(){
+  var xReady = document.getElementById("player-x-ready");
+  addClass(xReady, "visible");
+  removeClass(xReady, "hidden");
+};
+
+//Show the div with the class name of 'player-o-ready'
+function showOReady(){
+  var oReady = document.getElementById("player-o-ready");
+  addClass(oReady, "visible");
+  removeClass(oReady, "hidden");
+};
+
+function playerToggle(){
   if(turnSwitch % 2 === 0){
-    var xReady = document.getElementById("player-x-ready");
-    addClass(xReady, "visible");
-    removeClass(xReady, "hidden");
-    var oIdle = document.getElementById("player-o-ready");
-    addClass(oIdle, "hidden");
-    removeClass(oIdle, "visible");
+    showXReady();
+    hideOReady();
   } else {
-    var oReady = document.getElementById("player-o-ready");
-    addClass(oReady, "visible");
-    removeClass(oReady, "hidden");
-    var xIdle = document.getElementById("player-x-ready");
-    addClass(xIdle, "hidden");
-    removeClass(xIdle, "visible");
+    showOReady();
+    hideXReady();
   };
 }
+
+function addWinnerMessage(symbol){
+  if(symbol === "X"){
+    var xWin = document.getElementById("player-x-wins");
+    removeClass(xWin, "hidden");
+    addClass(xWin, "visible");
+    //Increment X player wins by 1
+    xPlayerWins++
+    //Display x player wins in html
+    var xScore = document.getElementById("x-score");
+    xScore.innerHTML = xPlayerWins;
+  } else {
+    var oWin = document.getElementById("player-o-wins");
+    removeClass(oWin, "hidden");
+    addClass(oWin, "visible");
+    //Increment O player wins by 1
+    oPlayerWins++
+    //Display 0 player wins in html
+    var oScore = document.getElementById("o-score");
+    oScore.innerHTML = oPlayerWins;
+  };
+};
 
 //Initiate a new game - maintain scores and turnSwitch
 function newGame() {
@@ -212,6 +240,12 @@ function newGame() {
   var tile = document.getElementById("tile-3-3");
   tile.innerHTML = "";
 
+  //Clear tile font-colors
+  var tiles = document.getElementsByClassName("board-tile");
+  for(var i = 0; i < tiles.length; i++){
+    tiles[i].classList.remove("place-x");
+    tiles[i].classList.remove("place-o");
+  };
 
   //Run Setup Page to reinstantiate event listeners
   setUpPage();
